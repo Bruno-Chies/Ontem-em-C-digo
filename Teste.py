3a83c5807287cd49bf226ca0ea8e5dd0236571e5
BrunoChies <bruno.echies@gmail.com>
2026-08-20 16:06:23 -0300

    Futuro teste

--- Teste.py
+++ Teste.py
@@ -1,10 +1,21 @@
 import feedparser
+import requests
 
-# Testa baixar direto, sem envolver a sua função nem listas complexas
-url = 'https://tecnoblog.net/feed/'
-print(f"Testando baixar direto: {url}")
+# Testando com a do TudoCelular ou Tecnoblog para validar o código
+url = 'https://www.tudocelular.com/feed'
 
-feed = feedparser.parse(url)
+headers = {
+    'User-Agent': (
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+        'AppleWebKit/537.36 (KHTML, like Gecko) '
+        'Chrome/120.0.0.0 Safari/537.36'
+    )
+}
 
-print(f"Objeto retornado: {feed}")
-print(f"Quantidade de entries: {len(feed.entries)}")
+resposta = requests.get(url, headers=headers, verify=False)
+
+if resposta.status_code == 200:
+  feed = feedparser.parse(resposta.text)
+  print(f'Sucesso! Carregou {len(feed.entries)} notícias.')
+else:
+  print(f'Erro: {resposta.status_code}')
